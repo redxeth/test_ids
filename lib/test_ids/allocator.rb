251999f7e5f0878444c4c737c78a08564382bb59
info-rchitect <brian.caquelin@outlook.com>
2018-05-07 12:55:48 -0500

    moved bin removal from options to last possible spot so other API would still work

--- lib/test_ids/allocator.rb
+++ lib/test_ids/allocator.rb
@@ -129,15 +129,15 @@ module TestIds
       end
 
       # Update the supplied options hash that will be forwarded to the program generator
-      unless options.delete(:bin) == :none || config.send_to_ate == false
+      unless options.delete(:bin) == :none
         options[:bin] = bin['number']
         options[:bin_size] = bin['size']
       end
-      unless options.delete(:softbin) == :none || config.send_to_ate == false
+      unless options.delete(:softbin) == :none
         options[:softbin] = softbin['number']
         options[:softbin_size] = softbin['size']
       end
-      unless options.delete(:number) == :none || config.send_to_ate == false
+      unless options.delete(:number) == :none
         options[:number] = number['number']
         options[:number_size] = number['size']
       end
--- lib/test_ids/origen_testers/flow.rb
+++ lib/test_ids/origen_testers/flow.rb
@@ -1,12 +1,20 @@
 require 'origen_testers/flow'
 module OrigenTesters
   module Flow
+  
+    BIN_OPTS = [:bin, :softbin, :bin_size, :softbin_size, :number, :number_size]
+    
     # Override the flow.test method to inject our generated bin and
     # test numbers
     alias_method :_orig_test, :test
     def test(instance, options = {})
       if TestIds.configured? && options[:test_ids] != :notrack
         TestIds.current_configuration.allocator.allocate(instance, options)
+      end
+      if TestIds.current_configuration.send_to_ate == false
+        BIN_OPTS.each do |opt|
+          options.delete(opt)
+        end
       end
       _orig_test(instance, options)
     end

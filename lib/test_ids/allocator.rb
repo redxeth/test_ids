8ab12a9d110a61fd61b4ae74da680cb984bb8490
Priyavadan Kumar <priyavadan.kumar@nxp.com>
2018-05-16 05:52:06 -0700

    Fixes issue #14 and #15

--- lib/test_ids/allocator.rb
+++ lib/test_ids/allocator.rb
@@ -26,8 +26,19 @@ module TestIds
 
     def range_item(range, options)
       orig_options = options.dup
-      # Create an alias for the databse that stores the pointers per range
-      rangehash = store['pointers']['ranges'] ||= {}
+      # Now Check if the database (JSON file) exists.
+      # If file exists, load the database and create the alias for ['pointer']['ranges']
+      if file && File.exist?(file)
+        lines = File.readlines(file)
+        # Remove any header comment lines since these are not valid JSON
+        lines.shift while lines.first =~ /^\/\// && !lines.empty?
+        s = JSON.load(lines.join("\n"))
+        rangehash = s['pointers']['ranges']
+        rangehash = Hash[rangehash.map { |k, v| [k.to_sym, v] }]
+      else
+        # Create an alias for the databse that stores the pointers per range
+        rangehash = store['pointers']['ranges'] ||= {}
+      end
       # Check the database to see if the passed in range has already been included in the database hash
       if rangehash.key?(:"#{range}")
         # Read out the database hash to see what the last_softbin given out was for that range.
